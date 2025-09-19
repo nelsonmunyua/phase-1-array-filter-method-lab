36a48ea78c4f8e1825b23b3b91d63a0083f91420
Nelson Munyua <nelsonmunyua8@gmail.com>
2025-09-19 23:42:03 +0300

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, letter) {
+    return drivers.filter(driver => driver.startsWith(letter))
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name)
+}
